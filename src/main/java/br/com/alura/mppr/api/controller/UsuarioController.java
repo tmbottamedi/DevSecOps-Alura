e543f27b74d77d9c8edc4e7a08bf101952c8e501
Tiago Mendes Bottamedi <tmbottamedi@mppr.mp.br>
2025-06-04 16:02:49 -0300

    Adicionando primeiros arquivos do projeto

---
+++ src/main/java/br/com/alura/mppr/api/controller/UsuarioController.java
@@ -0,0 +1,16 @@
+package br.com.alura.mppr.api.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api")
+public class UsuarioController {
+
+    @RequestMapping("teste")
+    public String teste() {
+        return "Bem vindo a API";
+    }
+    
+}
